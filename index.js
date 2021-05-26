fdcc97f2bf9cd1aa718d02b3bd38ccd12af48add
Prince0297Ahirwar <princeahirwar51@gmail.com>
2021-05-26 19:09:05 +0530

    add view engine and views path

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 8000;
 //using routes for routing
 app.use('/',require('./routes'));
 
+//setting view engine
+app.set("view engine",'ejs');
+app.set("views",'./views');
+
 
 app.listen(port,function(err){
     if(err){
