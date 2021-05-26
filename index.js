b861a9e3dc7e97719d5fc859eb37b1505d6825b6
Prince0297Ahirwar <princeahirwar51@gmail.com>
2021-05-26 17:31:33 +0530

    added router and controller

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = 8000;
 
+
+//using routes for routing
+app.use('/',require('./routes'));
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
