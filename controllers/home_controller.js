00a148c84e68a027056ec1638fea65bb3ab0bd86
Prince0297Ahirwar <princeahirwar51@gmail.com>
2021-05-30 23:26:36 +0530

    add nested population to view user and post details

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 
 
+const { populate } = require('../models/post');
 const Post = require('../models/post');
 
 module.exports.home = function(req,res){
@@ -14,7 +15,17 @@ module.exports.home = function(req,res){
         
     // })
 
-    Post.find({}).populate('user').exec(
+
+    //populate using nestin to populate first populate user in post and then populate comments and then populating user in each comments by nexting and specifying path
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(
         function(err,posts){
             if(err){console.log("error in retrieving posts",err); return}
             return res.render('home',{
