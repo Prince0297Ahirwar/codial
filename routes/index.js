398c1572fd9d802dd904dae2b44b509e96b03056
Prince0297Ahirwar <princeahirwar51@gmail.com>
2021-06-03 12:44:23 +0530

    add routes controller for api render api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const homeController = require('../controllers/home_controller');
-
 const router  = express.Router();
+
+
+
 
 router.get('/',homeController.home);
 
@@ -10,6 +12,9 @@ router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 
 router.use('/comments',require('./comments'));
+
+
+router.use('/api',require('./api'));
 //for any other further routes 
 //router.use('/routername',require('./router'));
 module.exports = router;
